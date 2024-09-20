a0c7ef90094a9ab568864e8d5edce2a8aec53dea
milenalilova <milenalilova78@gmail.com>
2024-09-20 12:45:02 -0300

    Added models, urls and views

--- urlsAndViews/urlsAndViews/departments/urls.py
+++ urlsAndViews/urlsAndViews/departments/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from urlsAndViews.departments import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('departments/', views.show_departments_by_name, name='show departments by name'),
+    path('departments/<int:pk>/', views.show_departments_by_id, name='show departments by id'),
 ]
--- urlsAndViews/urlsAndViews/departments/views.py
+++ urlsAndViews/urlsAndViews/departments/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from urlsAndViews.departments.models import Department
+
+
+def index(request):
+    return HttpResponse("<h1>This is the departments app.</h1>")
+
+
+def show_departments_by_name(request):
+    departments = Department.objects.all()
+    context = {
+        'departments': departments
+    }
+
+    return render(request, 'departments/show_departments_by_name.html', context)
+
+
+def show_departments_by_id(request, pk):
+    try:
+        department = Department.objects.get(pk=pk)
+    except Department.DoesNotExist:
+        return redirect('index')
+
+    context = {
+        'department': department
+    }
+
+    return render(request, 'departments/show_department_by_id.html', context)
