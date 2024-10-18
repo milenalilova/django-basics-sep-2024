fbee3ae93a54e2d033c601b9f242833c461fca43
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-10-18 17:43:12 -0300

    Delete classBasedViewsAdvanced/employeesApp directory

--- classBasedViewsAdvanced/employeesApp/employeesApp/common/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from employeesApp.common.views import IndexView, RedirectHomeView
-
-urlpatterns = [
-    path('', IndexView.as_view(), name='home page'),
-    path('redirect-home/', RedirectHomeView.as_view(), name='redirect home page'),
-]
